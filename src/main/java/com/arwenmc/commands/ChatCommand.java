93f88f31121f54b01c5f23eb67f285ae61c2a0b1
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 14:10:25 +0200

    Implemented methods

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -1,13 +1,25 @@
 package com.arwenmc.commands;
 
 import com.arwenmc.StaffChat;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
+
+import java.util.List;
 
 public class ChatCommand implements CommandExecutor, TabExecutor {
 
     StaffChat plugin;
     public ChatCommand(StaffChat plugin) {
         this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
+        return false;
+    }
+
+    public List<String> onTabComplete(CommandSender commandSender, Command command, String label, String[] args) {
+        return null;
     }
 }
