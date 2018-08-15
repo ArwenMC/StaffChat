1bd8c16921697ac1e252543962da55573ab83f10
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:37:54 +0200

    Created MainClass

---
+++ src/main/java/com/arwenmc/StaffChat.java
@@ -0,0 +1,17 @@
+package com.arwenmc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StaffChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
