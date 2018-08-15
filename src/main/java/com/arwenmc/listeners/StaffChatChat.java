cea1348118d9181dcf8d37841a27140de674dc74
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:46:06 +0200

    Added StaffChatChat - mointers chat events and disables them if they are meant for admin / staff

---
+++ src/main/java/com/arwenmc/listeners/StaffChatChat.java
@@ -0,0 +1,14 @@
+package com.arwenmc.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+
+public class StaffChatChat implements Listener {
+
+    @EventHandler
+    public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
+        
+    }
+
+}
