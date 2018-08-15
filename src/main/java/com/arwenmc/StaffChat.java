712c855c221539aeb7d082c0a66be94006dfd69d
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:47:03 +0200

    Added the array lists to keep track of players

--- src/main/java/com/arwenmc/StaffChat.java
+++ src/main/java/com/arwenmc/StaffChat.java
@@ -5,10 +5,17 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class StaffChat extends JavaPlugin {
 
     PluginDescriptionFile pdfFile = this.getDescription();
     PluginManager pluginManager = this.getServer().getPluginManager();
+
+    public ArrayList<UUID> admin = new ArrayList<UUID>();
+    public ArrayList<UUID> staff = new ArrayList<UUID>();
+    public ArrayList<UUID> player = new ArrayList<UUID>();
 
     @Override
     public void onEnable() {
