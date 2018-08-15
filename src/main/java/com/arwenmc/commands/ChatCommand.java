5b98e3c7418cf6e626e92733c9a94824230aec5b
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 15:02:41 +0200

    TabComplete

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -1,11 +1,13 @@
 package com.arwenmc.commands;
 
 import com.arwenmc.StaffChat;
+import com.google.common.collect.Lists;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ChatCommand implements CommandExecutor, TabExecutor {
@@ -20,6 +22,16 @@ public class ChatCommand implements CommandExecutor, TabExecutor {
     }
 
     public List<String> onTabComplete(CommandSender commandSender, Command command, String label, String[] args) {
+        List<String> argOne = Arrays.asList("reload");
+        List<String> listFinal = Lists.newArrayList();
+
+        switch (args.length) {
+            case 1:
+                for (String s : argOne) {
+                    if (s.startsWith(args[0])) listFinal.add(s);
+                }
+                return listFinal;
+        }
         return null;
     }
 }
