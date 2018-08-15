154be7da6525d9a9a258c25c0b659440df393dd6
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 14:09:54 +0200

    TabExecutor baby

--- src/main/java/com/arwenmc/commands/ChatCommand.java
+++ src/main/java/com/arwenmc/commands/ChatCommand.java
@@ -2,8 +2,9 @@ package com.arwenmc.commands;
 
 import com.arwenmc.StaffChat;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.TabExecutor;
 
-public class ChatCommand implements CommandExecutor {
+public class ChatCommand implements CommandExecutor, TabExecutor {
 
     StaffChat plugin;
     public ChatCommand(StaffChat plugin) {
