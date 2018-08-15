173906f51cf550802a3e0d4462c319659b7eee40
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:53:11 +0200

    Added variable to reduce useless getting of unique id

--- src/main/java/com/arwenmc/listeners/StaffChatJoinQuit.java
+++ src/main/java/com/arwenmc/listeners/StaffChatJoinQuit.java
@@ -6,6 +6,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import java.util.UUID;
+
 public class StaffChatJoinQuit implements Listener {
 
     StaffChat plugin;
@@ -15,34 +17,36 @@ public class StaffChatJoinQuit implements Listener {
 
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
+        UUID playerUUID = event.getPlayer().getUniqueId();
         if (event.getPlayer().hasPermission("sc.admin")) {
             // add to admin group
-            plugin.admin.add(event.getPlayer().getUniqueId());
+            plugin.admin.add(playerUUID);
         } else if (event.getPlayer().hasPermission("sc.staff")) {
             // add to staff group
-            plugin.staff.add(event.getPlayer().getUniqueId());
+            plugin.staff.add(playerUUID);
         } else {
             // add to regular group
-            plugin.player.add(event.getPlayer().getUniqueId());
+            plugin.player.add(playerUUID);
         }
     }
 
     @EventHandler
     public void onPlayerQuitEvent(PlayerQuitEvent event) {
+        UUID playerUUID = event.getPlayer().getUniqueId();
         if (event.getPlayer().hasPermission("sc.admin")) {
-            if (plugin.admin.contains(event.getPlayer().getUniqueId())) {
+            if (plugin.admin.contains(playerUUID)) {
                 // remove to admin group
-                plugin.admin.remove(event.getPlayer().getUniqueId());
+                plugin.admin.remove(playerUUID);
             }
         } else if (event.getPlayer().hasPermission("sc.staff")) {
-            if (plugin.admin.contains(event.getPlayer().getUniqueId())) {
+            if (plugin.admin.contains(playerUUID)) {
                 // remove to staff group
-                plugin.staff.add(event.getPlayer().getUniqueId());
+                plugin.staff.add(playerUUID);
             }
         } else {
-            if (plugin.player.contains(event.getPlayer().getUniqueId())) {
+            if (plugin.player.contains(playerUUID)) {
                 // remove to regular group
-                plugin.player.add(event.getPlayer().getUniqueId());
+                plugin.player.add(playerUUID);
             }
         }
     }
