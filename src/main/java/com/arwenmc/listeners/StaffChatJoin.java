623b9b31ce6acdb86b9c1e255b470d189f695005
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:48:34 +0200

    Adding people to the group

--- src/main/java/com/arwenmc/listeners/StaffChatJoin.java
+++ src/main/java/com/arwenmc/listeners/StaffChatJoin.java
@@ -1,17 +1,28 @@
 package com.arwenmc.listeners;
 
+import com.arwenmc.StaffChat;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class StaffChatJoin implements Listener {
 
+    StaffChat plugin;
+    public StaffChatJoin(StaffChat plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event) {
         if (event.getPlayer().hasPermission("sc.admin")) {
             // add to admin group
+            plugin.admin.add(event.getPlayer().getUniqueId());
         } else if (event.getPlayer().hasPermission("sc.staff")) {
             // add to staff group
+            plugin.staff.add(event.getPlayer().getUniqueId());
+        } else {
+            // add to regular group
+            plugin.player.add(event.getPlayer().getUniqueId());
         }
     }
 
