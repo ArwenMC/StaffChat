20c31eadc7473c8cc04962fbe07acc24b33c6244
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:39:51 +0200

    Added enable and disable messages

--- src/main/java/com/arwenmc/StaffChat.java
+++ src/main/java/com/arwenmc/StaffChat.java
@@ -1,17 +1,20 @@
 package com.arwenmc;
 
+import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StaffChat extends JavaPlugin {
 
+    PluginDescriptionFile pdfFile = this.getDescription();
+
     @Override
     public void onEnable() {
-        
+        getLogger().info("StaffChat v" + pdfFile.getVersion() + " has been enabled.");
     }
 
     @Override
     public void onDisable() {
-
+        getLogger().info("StaffChat v" + pdfFile.getVersion() + " has been disabled.");
     }
 
 }
