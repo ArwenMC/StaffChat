19138863bbccd7c6d2fe9e3ce4a6dbbeef37503d
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 15:20:55 +0200

    Added nkt player and no_permissions

--- src/main/java/com/arwenmc/StaffChat.java
+++ src/main/java/com/arwenmc/StaffChat.java
@@ -2,6 +2,7 @@ package com.arwenmc;
 
 import com.arwenmc.commands.ChatCommand;
 import com.arwenmc.listeners.StaffChatJoinQuit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
@@ -14,7 +15,9 @@ public class StaffChat extends JavaPlugin {
 
     PluginDescriptionFile pdfFile = this.getDescription();
     PluginManager pluginManager = this.getServer().getPluginManager();
+    FileConfiguration config = this.getConfig();
 
+    // Array List
     public ArrayList<UUID> admin = new ArrayList<UUID>();
     public ArrayList<UUID> staff = new ArrayList<UUID>();
     public ArrayList<UUID> player = new ArrayList<UUID>();
@@ -25,6 +28,9 @@ public class StaffChat extends JavaPlugin {
     public Permission scAdmin = new Permission("sc.admin");
     public Permission scStaff = new Permission("sc.staff");
 
+    public String NOT_PLAYER = config.getString("general.not_player");
+    public String NO_PERMISSION = config.getString("general.no_permission");
+    
     @Override
     public void onEnable() {
         getLogger().info("StaffChat v" + pdfFile.getVersion() + " has been enabled.");
