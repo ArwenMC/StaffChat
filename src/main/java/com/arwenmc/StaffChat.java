78556c7a68738182161c74b2e309e12974cd04dc
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-15 13:43:53 +0200

    Registering StaffChatJoin for events

--- src/main/java/com/arwenmc/StaffChat.java
+++ src/main/java/com/arwenmc/StaffChat.java
@@ -1,15 +1,20 @@
 package com.arwenmc;
 
+import com.arwenmc.listeners.StaffChatJoin;
 import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class StaffChat extends JavaPlugin {
 
     PluginDescriptionFile pdfFile = this.getDescription();
+    PluginManager pluginManager = this.getServer().getPluginManager();
 
     @Override
     public void onEnable() {
         getLogger().info("StaffChat v" + pdfFile.getVersion() + " has been enabled.");
+
+        pluginManager.registerEvents(new StaffChatJoin(), this);
     }
 
     @Override
