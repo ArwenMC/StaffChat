94a79d641d9f854175185bfc0be4b5101073e3a7
Christopher Nethercott <ccnethercott@gmail.com>
2018-08-16 16:56:05 +0200

    This is some basic structure i'd like to improve

--- src/main/java/com/arwenmc/listeners/SCListener.java
+++ src/main/java/com/arwenmc/listeners/SCListener.java
@@ -2,15 +2,28 @@ package com.arwenmc.listeners;
 
 import com.arwenmc.StaffChat;
 import com.arwenmc.util.ChannelType;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-public class StaffChatChat implements Listener {
+import java.util.UUID;
+
+public class SCListener implements Listener {
 
     @EventHandler
     public void onPlayerChatEvent(AsyncPlayerChatEvent event) {
         ChannelType type = new StaffChat().chatChannel.get(event.getPlayer().getUniqueId());
+
+        if (type.equals(ChannelType.ADMIN)) {
+            for (UUID uuid : new StaffChat().player) {
+                event.getRecipients().remove(Bukkit.getPlayer(uuid));
+            }
+            for (UUID uuid : new StaffChat().admin) {
+                event.getRecipients().add(Bukkit.getPlayer(uuid));
+            }
+        }
     }
 
 }
