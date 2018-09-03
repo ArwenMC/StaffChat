c2f96a971692206142d8debede40d53ab74fd4dc
Christopher Nethercott <ccnethercott@gmail.com>
2018-09-03 20:48:22 +0100

    Get players method - clunky but works

--- src/main/java/com/arwenmc/util/ChannelType.java
+++ src/main/java/com/arwenmc/util/ChannelType.java
@@ -1,6 +1,8 @@
 package com.arwenmc.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public enum ChannelType {
@@ -21,6 +23,16 @@ public enum ChannelType {
     // TODO add method to get ChannelType and therefor channel from hashmap
     public ChannelType getChannelHashMap(HashMap<UUID, ChannelType> hashMap, UUID uuid) {
         return hashMap.get(uuid);
+    }
+
+    public ArrayList<UUID> getPlayers(Map<UUID, ChannelType> channelList, ChannelType channelType) {
+        ArrayList<UUID> tmp = new ArrayList<>();
+        for (Map.Entry<UUID, ChannelType> channelTypeEntry : channelList.entrySet()) {
+            if (channelTypeEntry.equals(channelType)) {
+                tmp.add(channelTypeEntry.getKey());
+            }
+        }
+        return tmp;
     }
 
     public String getChannel(ChannelType channelType) {
